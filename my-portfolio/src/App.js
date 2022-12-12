545e532713629a6019a218e0c60ce4e226f04365
EgorYakushik <egor.yakushik@mail.ru>
2022-12-13 02:14:56 +0300

    projects page done

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,21 +1,19 @@
 import "./styles/main.css";
 
-
 import Nav from "./components/nav/Nav";
-import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
         <Nav />
 
-        <Header />
-
+        {/* <Home /> */}
+        <Projects />
         <Footer />
-        
 
-        
     </div>
   );
 }
