8b93c569a633b74be228e126a8b530b915742e26
EgorYakushik <egor.yakushik@mail.ru>
2022-12-13 19:06:48 +0300

    education, skills, contacts pages done

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,6 +4,9 @@ import Nav from "./components/nav/Nav";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
+import Education from "./pages/Education";
+import Skills from "./pages/Skills";
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
 
         {/* <Home /> */}
         <Projects />
+        {/* <Contacts /> */}
+        {/* <Education /> */}
+        {/* <Skills /> */}
+
         <Footer />
 
     </div>
---
+++ my-portfolio/src/pages/Education.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Education() {
+  return (
+    <main className="section">
+        <div className="container">
+            <h1 className="title-1">Education</h1>
+            <ul className="content-list">
+                <li className="content-list__item">
+                    <h2 className="title-2">High education</h2>
+                    <p>Belarusian National Technical University</p>
+
+                </li>
+                <li className="content-list__item">
+                    <h2 className="title-2">Сourses</h2>
+                    <p> <a href="https://myitschool.by/">Моя-Айти-Школа:</a></p>
+                    <p>Front-end web-development: HTML, CSS, JavaScript, ReactJS, Redux, ...</p>
+                </li>
+            </ul>
+        </div>    
+    </main>
+  )
+}
